e2a49fca08ce159302e415bd355c6fac3fe80cb7
Gemi Yudhia <githuba122@gmail.com>
2026-08-23 14:37:13 +0700

    fix ssl

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,6 +7,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
     });
     super({ adapter });
   }
